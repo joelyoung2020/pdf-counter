251a52c4d52265a6ea863aeafe66b8de5242567e
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-07-04 16:03:40 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 import express from "express";
-import mongoose from "mongoose";
-import cors from 'cors';
 import bodyParser from 'body-parser';
 import processPDF from "./wordCount.js";
 import upload from "./multer.js";
-import fs from "fs";
 
 const app = express()
 const PORT = 3000
@@ -12,7 +9,6 @@ const PORT = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
 
 app.post("/analyze-pdf", 
   upload.single('file'), 
@@ -20,7 +16,6 @@ app.post("/analyze-pdf",
     try {
       const file = req.file;
       const data = await processPDF(file.path);
-      // const result = await JSON.stringify(data)
       return res.status(200).json({
         success: true,
         result: data,
@@ -33,15 +28,6 @@ app.post("/analyze-pdf",
     }
 })
 
-const connect = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/wordcount")
-    app.listen(PORT, () => {
-      console.log(`Running on ${PORT}`)
-    })
-  } catch (error) {
-    console.log(error.message)
-  }
-}
-
-connect();
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`)
+})
--- wordCount.js
+++ wordCount.js
@@ -1,7 +1,7 @@
  import keyword_extractor from "keyword-extractor";
-import pos from 'pos';
+// import pos from 'pos';
 import { PdfReader } from "pdfreader";
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
 
 const processPDF = async (filePath) => {
@@ -47,38 +47,38 @@ const getKeywords = (string) => {
 }
 
 
-const filterUnwantedWords = (pdfString) => {
-  const words = new pos.Lexer().lex(pdfString)
-  const tagger = new pos.Tagger();
-  const taggedWords = tagger.tag(words);
+// const filterUnwantedWords = (pdfString) => {
+//   const words = new pos.Lexer().lex(pdfString)
+//   const tagger = new pos.Tagger();
+//   const taggedWords = tagger.tag(words);
 
-  const wantedWords = []
-  const unwantedTags = [
-    'DT',   'VBZ', 'DT',  'CC',
-    'MD',   'IN', 'MD',
-    'WRB',  'RB',  'PRP', 'PRP',
-    'PRP$', 'PRP', 'DT',  'IN',
-    'PRP',  '"',   'NN',  'PRP',
-    'PRP',  'PRP', '.', 'TO',
-    'VBP', 'VBD', 'WP', 'EX'
-  ]
+//   const wantedWords = []
+//   const unwantedTags = [
+//     'DT',   'VBZ', 'DT',  'CC',
+//     'MD',   'IN', 'MD',
+//     'WRB',  'RB',  'PRP', 'PRP',
+//     'PRP$', 'PRP', 'DT',  'IN',
+//     'PRP',  '"',   'NN',  'PRP',
+//     'PRP',  'PRP', '.', 'TO',
+//     'VBP', 'VBD', 'WP', 'EX'
+//   ]
 
-  // filter based on part of speech tag
-  for (let i = 0; i < taggedWords.length; i++) {
-    const taggedWord = taggedWords[i];
-    const word = taggedWord[0];
-    const tag = taggedWord[1];
+//   // filter based on part of speech tag
+//   for (let i = 0; i < taggedWords.length; i++) {
+//     const taggedWord = taggedWords[i];
+//     const word = taggedWord[0];
+//     const tag = taggedWord[1];
 
-    if(unwantedTags.includes(tag)) continue;
+//     if(unwantedTags.includes(tag)) continue;
 
-    // filter numbers
-    const isNumber = parseInt(word)
-    if (!isNaN(isNumber)) continue;
+//     // filter numbers
+//     const isNumber = parseInt(word)
+//     if (!isNaN(isNumber)) continue;
 
-    wantedWords.push(word)
-  }
-  return wantedWords;
-}
+//     wantedWords.push(word)
+//   }
+//   return wantedWords;
+// }
 
 
 const getMostCounted = async(wordArray) => {
@@ -102,47 +102,47 @@ const getMostCounted = async(wordArray) => {
 }
 
 const arrangeResults = async (firstTen) => {
-  const firstURL = await fetchWikipediaPageLink(firstTen[0][0])
-  const secondURL = await fetchWikipediaPageLink(firstTen[1][0])
-  const thirdURL = await fetchWikipediaPageLink(firstTen[2][0])
-  const fourthURL = await fetchWikipediaPageLink(firstTen[3][0])
-  const fifthURL = await fetchWikipediaPageLink(firstTen[4][0])
-  const sixthURL = await fetchWikipediaPageLink(firstTen[5][0])
-  const seventhURL = await fetchWikipediaPageLink(firstTen[6][0])
-  const eighthURL = await fetchWikipediaPageLink(firstTen[7][0])
-  const ninthURL = await fetchWikipediaPageLink(firstTen[8][0])
-  const tenthURL = await fetchWikipediaPageLink(firstTen[9][0])
+  // const firstURL = await fetchWikipediaPageLink(firstTen[0][0])
+  // const secondURL = await fetchWikipediaPageLink(firstTen[1][0])
+  // const thirdURL = await fetchWikipediaPageLink(firstTen[2][0])
+  // const fourthURL = await fetchWikipediaPageLink(firstTen[3][0])
+  // const fifthURL = await fetchWikipediaPageLink(firstTen[4][0])
+  // const sixthURL = await fetchWikipediaPageLink(firstTen[5][0])
+  // const seventhURL = await fetchWikipediaPageLink(firstTen[6][0])
+  // const eighthURL = await fetchWikipediaPageLink(firstTen[7][0])
+  // const ninthURL = await fetchWikipediaPageLink(firstTen[8][0])
+  // const tenthURL = await fetchWikipediaPageLink(firstTen[9][0])
 
   const result = [
     {
-      1: {word: firstTen[0][0], times: firstTen[0][1], pageURL: firstURL}
+      1: {word: firstTen[0][0], times: firstTen[0][1]}
     },
     {
-      2: {word: firstTen[1][0], times: firstTen[1][1], pageURL: secondURL}
+      2: {word: firstTen[1][0], times: firstTen[1][1]}
     },
     {
-      3: {word: firstTen[2][0], times: firstTen[2][1], pageURL: thirdURL}
+      3: {word: firstTen[2][0], times: firstTen[2][1]}
     },
     {
-      4: {word: firstTen[3][0], times: firstTen[3][1], pageURL: fourthURL}
+      4: {word: firstTen[3][0], times: firstTen[3][1]}
     },
     {
-      5: {word: firstTen[4][0], times: firstTen[4][1], pageURL: fifthURL}
+      5: {word: firstTen[4][0], times: firstTen[4][1]}
     },
     {
-      6: {word: firstTen[5][0], times: firstTen[5][1], pageURL: sixthURL}
+      6: {word: firstTen[5][0], times: firstTen[5][1]}
     },
     {
-      7: {word: firstTen[6][0], times: firstTen[6][1], pageURL: seventhURL}
+      7: {word: firstTen[6][0], times: firstTen[6][1]}
     },
     {
-      8: {word: firstTen[7][0], times: firstTen[7][1], pageURL: eighthURL}
+      8: {word: firstTen[7][0], times: firstTen[7][1]}
     },
     {
-      9: {word: firstTen[8][0], times: firstTen[8][1], pageURL: ninthURL}
+      9: {word: firstTen[8][0], times: firstTen[8][1]}
     },
     {
-      10: {word: firstTen[9][0], times: firstTen[9][1], pageURL: tenthURL}
+      10: {word: firstTen[9][0], times: firstTen[9][1]}
     },
   ]
 
@@ -151,32 +151,32 @@ const arrangeResults = async (firstTen) => {
 
 
 // Function to fetch the link to a Wikipedia page
-async function fetchWikipediaPageLink(pageTitle) {
-  const apiUrl = 'https://en.wikipedia.org/w/api.php';
-  const params = new URLSearchParams({
-    action: 'query',
-    format: 'json',
-    prop: 'info',
-    titles: pageTitle,
-    inprop: 'url'
-  });
-  const url = `${apiUrl}?${params.toString()}`;
+// async function fetchWikipediaPageLink(pageTitle) {
+//   const apiUrl = 'https://en.wikipedia.org/w/api.php';
+//   const params = new URLSearchParams({
+//     action: 'query',
+//     format: 'json',
+//     prop: 'info',
+//     titles: pageTitle,
+//     inprop: 'url'
+//   });
+//   const url = `${apiUrl}?${params.toString()}`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
 
-    // Extract the page link
-    const pageId = Object.keys(data.query.pages)[0];
-    const pageData = data.query.pages[pageId];
-    const pageUrl = pageData.fullurl;
+//     // Extract the page link
+//     const pageId = Object.keys(data.query.pages)[0];
+//     const pageData = data.query.pages[pageId];
+//     const pageUrl = pageData.fullurl;
 
-    return pageUrl;
-  } catch (error) {
-    console.error('Error fetching Wikipedia page link:', error);
-    throw error;
-  }
-}
+//     return pageUrl;
+//   } catch (error) {
+//     console.error('Error fetching Wikipedia page link:', error);
+//     throw error;
+//   }
+// }
 
 
 export default processPDF;
